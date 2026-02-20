0bd1e753b637902a4166a3eb2f89f8ee53f8d50b
Fabrizio <fabriziodisciorio91@gmail.com>
2026-02-20 15:13:29 +0100

    Update __init__.py

--- lambdaguard/__init__.py
+++ lambdaguard/__init__.py
@@ -1,6 +1,9 @@
+__version__ = "0.1.0"
+
 from .ofi import generalization_index, instability_index, create_model, run_experiment_multi_model, plot_all_multi_model, regression_test
 from .lambda_guard import lambda_guard_test, boosting_leverage, interpret
 from .cusum import lambda_detect
+
 __all__ = [
     "generalization_index",
     "instability_index",
