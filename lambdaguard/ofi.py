e45e27f43d1e38c96439e793f64e62e561a7b018
Fabrizio <fabriziodisciorio91@gmail.com>
2026-03-13 15:21:24 +0100

    Update ofi.py

--- lambdaguard/ofi.py
+++ lambdaguard/ofi.py
@@ -7,6 +7,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from itertools import product
+import statsmodels.api as sm
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
